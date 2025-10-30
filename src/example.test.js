8861a7071232e512feb866d37c68ff0e2471042b
Jonathan Becker <masteraikeus@gmail.com>
2025-10-29 22:47:39 -0500

    update test with migration

--- src/example.test.js
+++ src/example.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { execute } = require('./db');
 
 describe('example', () => {
   it('should be integrated', async () => {
@@ -7,13 +8,32 @@ describe('example', () => {
     process.env.MYSQL2_PASSWORD = 'password';
     process.env.MYSQL2_DATABASE = 'dbmate_demo';
 
+    console.log(process.env)
+    if (process.env.NODE_ENV === 'circle') {
+      await execute({
+        sql: `DROP TABLE IF EXISTS example`,
+        params: {}
+      });
+      await execute({
+        sql: `
+            CREATE TABLE example
+            (
+                uuid      VARCHAR(36)  NOT NULL,
+                name      VARCHAR(255) NOT NULL,
+                createdAt DATETIME(3)  NOT NULL,
+                updatedAt DATETIME(3)  NOT NULL
+            ) CHARSET utf8mb4
+              ENGINE = InnoDB`
+      });
+    }
+
+    console.log('table creation successful');
+
     const app = require('./index');
     const res = await request(app)
       .post('/')
       .set("Content-Type", "application/json")
       .send({ name: 'test' });
-
-    console.log(process.env)
 
     console.log(res);
 
