e5c99d0c2857f981c496988f8a2308ef27eefd9b
Jonathan Becker <masteraikeus@gmail.com>
2025-11-01 23:28:49 -0500

    test db migrations

--- src/example.test.js
+++ src/example.test.js
@@ -8,24 +8,24 @@ describe('example', () => {
     process.env.MYSQL2_DATABASE = 'dbmate_demo';
 
     const { execute } = require('./db');
-    if (process.env.NODE_ENV === 'circle') {
-      await execute({
-        sql: `DROP TABLE IF EXISTS example`,
-        params: {}
-      });
-      await execute({
-        sql: `
-            CREATE TABLE example
-            (
-                uuid      VARCHAR(36)  NOT NULL,
-                name      VARCHAR(255) NOT NULL,
-                createdAt DATETIME(3)  NOT NULL,
-                updatedAt DATETIME(3)  NOT NULL
-            ) CHARSET utf8mb4
-              ENGINE = InnoDB`
-      });
-      console.log('table creation successful');
-    }
+    // if (process.env.NODE_ENV === 'circle') {
+    //   await execute({
+    //     sql: `DROP TABLE IF EXISTS example`,
+    //     params: {}
+    //   });
+    //   await execute({
+    //     sql: `
+    //         CREATE TABLE example
+    //         (
+    //             uuid      VARCHAR(36)  NOT NULL,
+    //             name      VARCHAR(255) NOT NULL,
+    //             createdAt DATETIME(3)  NOT NULL,
+    //             updatedAt DATETIME(3)  NOT NULL
+    //         ) CHARSET utf8mb4
+    //           ENGINE = InnoDB`
+    //   });
+    //   console.log('table creation successful');
+    // }
 
     const app = require('./index');
     const res = await request(app)
